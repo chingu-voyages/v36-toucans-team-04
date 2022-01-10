567f68adb0e76e3d9c170726a633edf7d490b9a0
Michael Park <mpark4656@gmail.com>
2022-01-09 19:06:28 -0500

    Added jQuery and Bootstrap files to lib. Refactored index.js to use jQuery. Removed Howler sound play on mouseenter on menu items. Instead, the sound will play on menu item click. Redesigned the landing page.

--- assets/common/js/index.js
+++ assets/common/js/index.js
@@ -1,13 +1,25 @@
+// Create a mouse click sound
 let sound = new Howl({
     src: ["./assets/audio/mixkit-select-click-1109.wav"]
 });
 
-let listItems = document.querySelectorAll("li");
-for(listItem of listItems) {
-    listItem.addEventListener("mouseenter", function() {
-        sound.play();
-    });
-}
+// Animate the header and menu items upon document loading
+$("header").addClass("animate-slidein-right");
+$("main").addClass("animate-slidein-left");
 
-document.querySelector("header").classList.add("animate-slidein-left-right");
-document.querySelector("main").classList.add("animate-slidein-right-left");
+// Add event listners to the menu items
+$(".menu-item").on("click", function() {
+    // Play the click sound
+    sound.play();
+
+    $("header").removeClass("animate-slidein-right");
+    $("header").addClass("animate-slideout-left");
+
+    $("main").removeClass("animate-slidein-left");
+    $("main").addClass("animate-slideout-right");
+
+    setTimeout(() => {
+        let link = $(this).attr("data-href");
+        window.location.href = link;
+    }, 500)
+});
