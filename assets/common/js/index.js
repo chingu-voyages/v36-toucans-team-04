a52db3d6ac034b45c0f600acde7e403c44c0a579
Michael Park <mpark4656@gmail.com>
2022-01-10 22:01:18 -0500

    Added Animate.css to the project and incorporated it into the landing page

--- assets/common/js/index.js
+++ assets/common/js/index.js
@@ -3,23 +3,16 @@ let sound = new Howl({
     src: ["assets/audio/mixkit-select-click-1109.wav"]
 });
 
-// Animate the header and menu items upon document loading
-$("header").addClass("animate-slidein-right");
-$("main").addClass("animate-slidein-left");
-
 // Add event listners to the menu items
 $(".menu-item").on("click", function() {
     // Play the click sound
     sound.play();
+    
+    $("header").addClass("animate__animated animate__bounceOutRight");
+    $("main").addClass("animate__animated animate__bounceOutLeft");
 
-    $("header").removeClass("animate-slidein-right");
-    $("header").addClass("animate-slideout-left");
-
-    $("main").removeClass("animate-slidein-left");
-    $("main").addClass("animate-slideout-right");
-
-    setTimeout(() => {
+    $("main").on("animationend", () => {
         let link = $(this).attr("data-href");
         window.location.href = link;
-    }, 500)
+    });
 });
