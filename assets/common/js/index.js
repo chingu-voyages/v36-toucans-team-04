bd1f6b57169868d88fbf2686f52ae154df54c5aa
Michael Park <mpark4656@gmail.com>
2022-01-19 21:13:09 -0500

    Revert "Redesign the landing page and add missing files for Bootstrap"
    This reverts commit fabed7dd09e4289c455643c7456ba34ff9599fb1.

--- assets/common/js/index.js
+++ assets/common/js/index.js
@@ -1,10 +1,18 @@
-$("#header").addClass("animate__animated animate__bounceInDown");
-$("main > div").addClass("animate__animated animate__bounceInUp");
-$(".btn").addClass("animate__animated animate__pulse animate__infinite infinite");
+// Create a mouse click sound
+let sound = new Howl({
+    src: ["assets/audio/mixkit-select-click-1109.wav"]
+});
 
-$("#header").on("animationend", function(event) {
-    if(event.originalEvent.animationName == "bounceInDown") {
-        $("#header").removeClass("animate__bounceInDown");
-        $("#header").addClass("animate__tada");
-    }
+// Add event listners to the menu items
+$(".menu-item").on("click", function() {
+    // Play the click sound
+    sound.play();
+    
+    $("header").addClass("animate__animated animate__bounceOutRight");
+    $("main").addClass("animate__animated animate__bounceOutLeft");
+
+    $("main").on("animationend", () => {
+        let link = $(this).attr("data-href");
+        window.location.href = link;
+    });
 });
